3ee6e3a279d4fffefd992b0e83aba9ac8d81bb45
Jovino Monterde <jovinobironmonterde@gmail.com>
2026-03-09 21:29:12 +0800

    Updated Profile V4

--- app/pages/AddInfo/page.tsx
+++ app/pages/AddInfo/page.tsx
@@ -567,7 +567,7 @@ export default function AddInfo() {
             </button>
           </div>
         ) : (
-          <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-4">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-4">
             {profiles.map((profile) => (
               <ProfileCard
                 key={profile.id}
